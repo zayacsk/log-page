b1955d680bdbf6666c0e23b3a5b9b4d999830ab7
denis2415 <98222602+zayacsk@users.noreply.github.com>
2022-10-19 14:09:13 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const login = "admin";
+const password = 'admin';
 
 app.use(cors());
+app.use(express.urlencoded());
+app.use(express.json());
 
 app.use('/login', (req, res) => {
+  console.log('reqBc', req.body)
+  if (req.body.username === login && req.body.password === password) {
     res.send({
       token: 'test123'
     });
+  } else {
+    res.send({
+      error: "мимо"
+    });
+  }
 });
 
 app.listen(8080, () => console.log('API is running on http://localhost:8080/login'));
